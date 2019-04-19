dcd763c7d81eaa99b43ca86fa8e58a0c3c4734b6
Valerie <valerietbourdon@gmail.com>
2019-04-19 14:09:15 -0400

    Path change

--- js/homelist.js
+++ js/homelist.js
@@ -2,7 +2,7 @@
 let i, j, x = "";
 
 for (i in projectList.projects) {
-  x += "<div class='projHover'><a href='projects/" + projectList.projects[i].index + ".html'>" + projectList.projects[i].name;
+  x += "<div class='projHover'><a href='" + projectList.projects[i].index + ".html'>" + projectList.projects[i].name;
   for (j in projectList.projects[i].tags) {
     x += "<div class='projTag'>" + " " + projectList.projects[i].tags[j] + "<img class='imageHover' src='img/" + projectList.projects[i].index + "/thumb.gif'>" + "</div></div></a>";
   }
