c832b2b80c086d8977d4629d3c809310bcdf0d20
Valerie <valerietbourdon@gmail.com>
2019-07-13 13:24:20 -0400

    added unlisted pages setting

--- js/aos.js
+++ js/aos.js
@@ -1,3 +1,5 @@
+//scroll
+
 AOS.init({
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 120, // offset (in px) from the original trigger point
--- js/homelist.js
+++ js/homelist.js
@@ -2,12 +2,13 @@
 let i, j, x = "";
 
 for (i in projectList.projects) {
-  // x += "<div class='projHover'><a href='" + projectList.projects[i].index + ".html'>" + projectList.projects[i].name;
-  x += "<div class='projHover'><a href='" + projectList.projects[i].index + ".html'>" + projectList.projects[i].name;
-  for (j in projectList.projects[i].tags) {
-    x += "<div class='projTag'>" + " " + projectList.projects[i].tags[j] + "<img class='imageHover' src='img/" + projectList.projects[i].index + "/thumb.gif'>" + "</div></div></a>";
+  if (projectList.projects[i].visible == true) {
+    x += "<div class='projHover'><a href='" + projectList.projects[i].index + ".html'>" + projectList.projects[i].name;
+    for (j in projectList.projects[i].tags) {
+      x += "<div class='projTag'>" + " " + projectList.projects[i].tags[j] + "<img class='imageHover' src='img/" + projectList.projects[i].index + "/thumb.gif'>" + "</div></div></a>";
+    }
+    x += "<br>";
   }
-  x += "<br>";
 }
 
 $(".projList").append(x);
